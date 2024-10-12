684f93d9874acf6bb967e9625147a1160060fc20
Ulugbek <mirzarakhmanoff@gmail.com>
2024-10-12 23:13:53 +0500

    add

--- src/redux/api/index.tsx
+++ src/redux/api/index.tsx
@@ -1,16 +1,16 @@
-import { createApi, fetchBaseQuery, retry } from '@reduxjs/toolkit/query/react'
+import { createApi, fetchBaseQuery, retry } from "@reduxjs/toolkit/query/react";
 // import { logout } from '../slices/authSlice';
 
 const baseQuery = async (args: any, api: any, extraOptions: any) => {
-  const { dispatch } = api
+  // const { dispatch } = api
   const rawBaseQuery = fetchBaseQuery({
     baseUrl: "https://dummyjson.com",
     prepareHeaders: (headers) => {
-      const token = localStorage.getItem("x-auth-token")
+      const token = localStorage.getItem("x-auth-token");
       if (token) {
-        headers.set('Authorization', `Bearer ${token}`)
+        headers.set("Authorization", `Bearer ${token}`);
       }
-      return headers
+      return headers;
     },
   });
 
@@ -19,17 +19,17 @@ const baseQuery = async (args: any, api: any, extraOptions: any) => {
   if (result.error) {
     const { status } = result.error;
     if (status === 401 || status === 403) {
-      console.error('Unauthorized access - Redirecting to login...');
+      console.error("Unauthorized access - Redirecting to login...");
       // dispatch(logout())
     }
   }
   return result;
 };
-const baseQueryWithRetry = retry(baseQuery, { maxRetries: 0 })
+const baseQueryWithRetry = retry(baseQuery, { maxRetries: 0 });
 
 export const api = createApi({
-  reducerPath: 'myApi',
+  reducerPath: "myApi",
   baseQuery: baseQueryWithRetry,
-  tagTypes: ["Product"], 
+  tagTypes: ["Product"],
   endpoints: () => ({}),
-})
+});
