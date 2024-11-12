5cf3059a95473c348c956de452398e82d3a6623a
Ulugbek <mirzarakhmanoff@gmail.com>
2024-11-12 21:50:05 +0500

    profile page responsive is done

--- src/pages/profile/Profile.tsx
+++ src/pages/profile/Profile.tsx
@@ -64,21 +64,25 @@ const Profile: React.FC = () => {
 
   return (
     <div className="bg-black text-white min-h-screen p-6 absolute md:left-[300px]">
-      <div className="flex items-center space-x-6 ">
-        <div className="md:w-20 md:h-20 w-14 h-12 border rounded-full bg-white overflow-hidden flex justify-center items-center">
-          <img
-            src={data?.photo?.includes("https") ? data.photo : avatar}
-            alt="Profile photo"
-            className="object-cover"
-          />
-        </div>
-
-        <div>
-          <div className="flex items-center justify-start ">
-            <h1 className=" font-bold text-sm md:text-2xl">{data?.fullName}</h1>
-            <FaCheckCircle className="text-blue-500" />
+      <div className="flex  space-x-6 flex-col md:flex-row items-start justify-center ">
+        <div className="flex gap-2 items-center justify-center ">
+          <div className="md:w-20 md:h-20 w-14 h-12 mb-3 border rounded-full bg-white overflow-hidden flex justify-center items-center">
+            <img
+              src={data?.photo?.includes("https") ? data.photo : avatar}
+              alt="Profile photo"
+              className="object-cover"
+            />
           </div>
-          <p className="text-gray-400 ">{`@ ${data?.username}`}</p>
+
+          <div>
+            <div className="flex items-center justify-start ">
+              <h1 className=" font-bold text-sm md:text-2xl">
+                {data?.fullName}
+              </h1>
+              <FaCheckCircle className="text-blue-500" />
+            </div>
+            <p className="text-gray-400 ">{`@ ${data?.username}`}</p>
+          </div>
         </div>
         {data._id === currentUser._id ? (
           <button className="flex items-center text-white rounded-lg px-4 py-2 transition duration-200 hover:text-[#FFA700] hover:shadow-lg">
@@ -86,7 +90,7 @@ const Profile: React.FC = () => {
             <span className="hidden  md:block"> Edit Profile</span>
           </button>
         ) : (
-          <div className="ml-auto space-x-4">
+          <div className=" ">
             {data.followers.some(
               (follower: any) => follower._id === currentUser._id
             ) ? (
@@ -104,7 +108,7 @@ const Profile: React.FC = () => {
                 Follow
               </button>
             )}
-            <button className="border border-gray-500 px-4 py-2 rounded-lg">
+            <button className="border border-gray-500 px-4 py-1 ml-[10px] rounded-lg">
               Message
             </button>
           </div>
